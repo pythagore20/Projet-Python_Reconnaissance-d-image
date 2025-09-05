5f400d25823b49393b0c92f84aac5d13cabbf955
pythagore20 <wahaboukabore3@gmail.com>
2025-09-05 02:19:26 +0100

    Mise à jour de predict_img et corrections dans le notebook

--- pred_alea_img.py
+++ pred_alea_img.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
 
-def predict_img(model, train_generator, train_dir, target_size=(64,64)):
+def predict_img(model, train_generator, train, target_size=(150,150)):
 
-    doss_choix = np.random.choice(os.listdir(train_dir))
-    img_nom = np.random.choice(os.listdir(os.path.join(train_dir, doss_choix)))
-    img_path = os.path.join(train_dir, doss_choix, img_nom)
+    doss_choix = np.random.choice(os.listdir(train))
+    img_nom = np.random.choice(os.listdir(os.path.join(train, doss_choix)))
+    img_path = os.path.join(train, doss_choix, img_nom)
     
     
     img = image.load_img(img_path, target_size=target_size)
@@ -15,11 +15,11 @@ def predict_img(model, train_generator, train_dir, target_size=(64,64)):
     x = np.expand_dims(x, axis=0)
     
 
-    prediction = model.predict(x)
+    pred = model.predict(x)
     
     class_indices = train_generator.class_indices
     idx_to_class = {i: j for j, i in class_indices.items()}  
-    pred_class = 0 if prediction[0][0] < 0.5 else 1
+    pred_class = 0 if pred[0][0] < 0.5 else 1
     pred_label = idx_to_class[pred_class]
     
    
@@ -27,7 +27,7 @@ def predict_img(model, train_generator, train_dir, target_size=(64,64)):
     plt.axis("off")
     plt.title(f"Prédit : {pred_label}")
     plt.show()
-    print(f"Valeur du modèle: {prediction[0][0]:.4f}")
+    print(f"Valeur du modèle: {pred[0][0]:.4f}")
     print(f"L'animal sur l'image est un : {pred_label}")
     
     return pred_label
